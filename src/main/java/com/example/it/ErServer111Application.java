003d73ce7c102a150dc9a6c74af796e9c7434ea8
Pandit Rathod <panditrathod95@gmail.com>
2022-04-10 09:09:22 +0530

    microdetails

---
+++ src/main/java/com/example/it/ErServer111Application.java
@@ -0,0 +1,17 @@
+package com.example.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ErServer111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErServer111Application.class, args);
+	}
+
+}
